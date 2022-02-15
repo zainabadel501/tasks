5259899c285b9cb4312bf3ced5ff44a8a9b98c41
zainabadel501 <92651816+zainabadel501@users.noreply.github.com>
2022-02-15 11:19:07 +0300

    changes in the index.html

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB71civMACTzBm7mrWDPF0t1mHD59KXpLM",
+    authDomain: "my-task-project-3c8eb.firebaseapp.com",
+    projectId: "my-task-project-3c8eb",
+    storageBucket: "my-task-project-3c8eb.appspot.com",
+    messagingSenderId: "569547331901",
+    appId: "1:569547331901:web:cb54e0aad4108e14044d54",
+    measurementId: "G-C5TZ2GE0YF"
+  }
 };
 
 
